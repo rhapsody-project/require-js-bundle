fd3073cefa7c981d548e42778210e51b4f49d52c
Sean Quinn <swquinn@gmail.com>
2014-05-20 23:56:57 -0400

    RequireJS 2.1.11 and current useful loader plugins.

---
+++ RhapsodyRequireJsBundle.php
@@ -0,0 +1,48 @@
+<?php
+/* Copyright (c) 2014 Rhapsody Project
+ *
+* Licensed under the MIT License (http://opensource.org/licenses/MIT)
+*
+* Permission is hereby granted, free of charge, to any
+* person obtaining a copy of this software and associated
+* documentation files (the "Software"), to deal in the
+* Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software,
+* and to permit persons to whom the Software is furnished
+* to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice
+* shall be included in all copies or substantial portions of
+* the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+* KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+* WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+* OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+* OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+* OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+namespace Rhapsody\RequireJsBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * A Symfony framework bundle which allows for the inclusion of RequireJS into
+ * any project.
+ *
+ * @author    Sean W. Quinn
+ * @category  Rhapsody RequireJsBundle
+ * @package   Rhapsody\RequireJsBundle
+ * @copyright Copyright (c) 2014 Rhapsody Project
+ * @license   http://opensource.org/licenses/MIT
+ * @version   $Id$
+ * @since     1.0
+ */
+class RhapsodyRequireJsBundle extends Bundle
+{
+
+}
